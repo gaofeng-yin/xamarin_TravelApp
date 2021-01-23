8a3361293b00438b27510d09348e3f32c11d4e40
GaofengYin <37072502+GaofengYin@users.noreply.github.com>
2021-01-23 15:05:46 +0000

    connecting to the app service

--- TravelRecordApp/TravelRecordApp/App.xaml.cs
+++ TravelRecordApp/TravelRecordApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,6 +8,8 @@ namespace TravelRecordApp
     public partial class App : Application
     {
         public static string DatabaseLocation = string.Empty;
+
+        public static MobileServiceClient client = new MobileServiceClient("https://travelrecordappx.azurewebsites.net");
         public App()
         {
             InitializeComponent();

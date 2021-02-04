98bf663abf9a017b8951ff3f7dcfdeef57e65dee
GaofengYin <37072502+GaofengYin@users.noreply.github.com>
2021-02-04 15:34:48 +0000

    command binding homepage

---
+++ TravelRecordApp/TravelRecordApp/ViewModel/Commands/NavigationCommands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace TravelRecordApp.ViewModel.Commands
+{
+    public class NavigationCommands : ICommand
+    {
+        public HomeVM HomeViewModel { get; set; }
+
+        public NavigationCommands(HomeVM homeVM)
+        {
+            HomeViewModel = homeVM;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            HomeViewModel.Navigate();
+        }
+    }
+}
---
+++ TravelRecordApp/TravelRecordApp/ViewModel/HomeVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelRecordApp.ViewModel.Commands;
+
+namespace TravelRecordApp.ViewModel
+{
+    public class HomeVM
+    {
+        public NavigationCommands NavCommand { get; set; }
+
+        public HomeVM()
+        {
+            NavCommand = new NavigationCommands(this);
+        }
+        public async void Navigate()
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new NewTravelPage());
+        }
+    }
+}

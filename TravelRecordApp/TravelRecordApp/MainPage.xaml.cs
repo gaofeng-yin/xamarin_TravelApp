6cda832b53b39f778b1390e0ba22c55c262a5efe
GaofengYin <37072502+GaofengYin@users.noreply.github.com>
2021-02-04 16:23:08 +0000

    loginPage to viewModel

--- TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -5,33 +5,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TravelRecordApp.Model;
+using TravelRecordApp.ViewModel;
 using Xamarin.Forms;
 
 namespace TravelRecordApp
 {
     public partial class MainPage : ContentPage
     {
+        MainVM viewModel;
         public MainPage()
         {
             InitializeComponent();
 
             var assembly = typeof(MainPage);
 
+            viewModel = new MainVM();
+            BindingContext = viewModel;
+
             iconImage.Source = ImageSource.FromResource("TravelRecordApp.Assets.Images.rocket.pjng", assembly);
-        }
-
-        private async void LoginButton_Clicked(object sender, EventArgs e)
-        {
-            bool canLogin = await Users.Login(emailEntry.Text, passwordEntry.Text);
-
-            if (canLogin)
-            {
-                await Navigation.PushAsync(new HomePage());
-            }
-            else
-            {
-                await DisplayAlert("Error", "Try again!", "OK");
-            }
         }
 
         private void registerUserButton_Clicked(object sender, EventArgs e)

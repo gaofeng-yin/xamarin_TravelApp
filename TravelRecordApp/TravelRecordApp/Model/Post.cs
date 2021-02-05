c3111a18a4388454dc2086d5ec1ad4c776365269
GaofengYin <37072502+GaofengYin@users.noreply.github.com>
2021-02-05 12:27:10 +0000

    converters: post createdat to string

--- TravelRecordApp/TravelRecordApp/Model/Post.cs
+++ TravelRecordApp/TravelRecordApp/Model/Post.cs
@@ -167,6 +167,18 @@ namespace TravelRecordApp.Model
             }
         }
 
+        private DateTimeOffset createdat;
+
+        public DateTimeOffset CREATEDAT
+        {
+            get { return createdat; }
+            set 
+            { 
+                createdat = value;
+                OnPropertyChnaged("CREATEDAT");
+            }
+        }
+
 
         public event PropertyChangedEventHandler PropertyChanged;
 
